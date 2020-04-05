536744296c067e5aeb318204fc54c0885341b906
Rafael <rafaelbuksa@gmail.com>
2020-04-05 16:00:11 +0300

    Done

---
+++ LocalizedSorting/LocalizedSorting/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace LocalizedSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> persoane = new List<string>();
+            string line;
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+
+                while (sr.Peek() > 0)
+                {
+                    line = sr.ReadLine();
+                    line = line.ToLower();
+                    TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+                    line = ti.ToTitleCase(line);
+                    persoane.Add(line);
+
+                }
+
+            }
+            persoane.Sort();
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                foreach (string pers in persoane)
+                {
+                    sw.WriteLine(pers);
+                }
+            }
+
+
+            Console.WriteLine(File.ReadAllText("output.txt"));
+
+
+        }
+    }
+}
